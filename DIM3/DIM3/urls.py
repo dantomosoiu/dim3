b04263bbbdb3030e67fee4f70cbff9d7fc425756
Dan <dan.tomosoiu@gmail.com>
2013-03-09 17:52:03 +0000

    index page kinda working

--- DIM3/DIM3/urls.py
+++ DIM3/DIM3/urls.py
@@ -25,6 +25,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('BerserkerChat.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
