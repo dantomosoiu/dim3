e4cf85b9e0b21f7d2bd5a7af296e43af7624fa28
Tony Lau <1102266l@student.gla.ac.uk>
2013-03-09 14:17:08 +0000

    added admin and berserkerchat app to settings.py

---
+++ dim3project/berserkerchat/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+#from rango.models import Category, Page
+
+#admin.site.register(Category)
+#admin.site.register(Page)
+
+#fix above to include models (not yet created)
