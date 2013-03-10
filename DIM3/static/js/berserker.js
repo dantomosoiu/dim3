6adc915871dc1e9029cc76b832093a74514a3e3d
Dan <dan.tomosoiu@gmail.com>
2013-03-10 06:37:07 +0000

    created frontend for supporting multiple tabs. need to instal djajax on backend

---
+++ DIM3/static/js/berserker.js
@@ -0,0 +1,44 @@
+var tabCounter = 0;
+
+$(document).ready(function () {
+
+    tabCounter = 3;
+
+    setTabSelectorClick();
+
+
+});
+
+
+
+var addTab = function (name, content) {
+    tabCounter++;
+    $('#tab-list > div').hide();
+    $('#tab-selector li').removeClass("active");
+    $('#tab-selector li').addClass("inactive");
+    $('#tab-selector ul').append("<li data-tab-selector='" + tabCounter + "' class=\"active\">" + name + "</li> ");
+    setTabSelectorClick();
+
+    var newTab = document.createElement("div");
+    $(newTab).html(content);
+    newTab.setAttribute("id", "tab-"+tabCounter);
+    $("#tab-list").append(newTab);
+    $(newTab).show();
+};
+
+
+var setTabSelectorClick = function(){
+    $('#tab-selector li').click(function (event) {
+        $('#tab-list > div').hide();
+        $('#tab-selector li').removeClass("active");
+        $('#tab-selector li').addClass("inactive");
+        $(event.target).removeClass("inactive");
+        $(event.target).addClass("active");
+
+        $('#tab-' + $(event.target).attr("data-tab-selector")).show();
+    });
+}
+
+function my_callback(data){
+    alert(data.message);
+}
